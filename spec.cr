7382227ad2b789f06916bd65d8ebe17f395d72e5
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-07-14 21:46:37 +0300

    fix

--- spec.cr
+++ spec.cr
@@ -9,9 +9,9 @@ describe Sophia do
     ind.add "o2", ["a", "b"]
     ind.add "o3", ["a", "b", "c"]
     ind.find(["a", "b", "c"], 2).should eq ["o3"]
-    ind.find(["a", "b"], 2).should eq ["o2", "o3"]
+    ind.find(["a", "b"]).should eq ["o2", "o3"]
     ind.find(["a", "b"], 1).should eq ["o2"]
-    ind.find(["a"], 3).should eq ["o1", "o2", "o3"]
+    ind.find(["a"]).should eq ["o1", "o2", "o3"]
     ind.find(["a"], 2).should eq ["o1", "o2"]
     ind.find(["a"], 1).should eq ["o1"]
   end
