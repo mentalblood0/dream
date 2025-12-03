2cada0d47aa68faa6aef5d783aa4431f6e91dcd7
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-12-03 16:31:15 +0300

    finish search method

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use xxhash_rust::xxh3::xxh3_128;
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Default, PartialEq, Debug, bincode::Encode, bincode::Decode)]
+#[derive(Clone, Default, PartialEq, PartialOrd, Debug, bincode::Encode, bincode::Decode)]
 pub struct Id {
     pub value: [u8; 16],
 }
@@ -225,59 +225,6 @@ impl<'a> WriteTransaction<'a> {
             .map(|((_, current_tag_id), _)| Ok(current_tag_id))
             .collect::<Vec<_>>()
     }
-
-    pub fn search(
-        &self,
-        present_tags: &Vec<Object>,
-        absent_tags: &Vec<Object>,
-        start_after_object: Option<Id>,
-    ) -> Result<Box<dyn FallibleIterator<Item = Id, Error = String> + '_>, String> {
-        Ok(Box::new(SearchIterator {
-            database_write_transaction: &self.database_write_transaction,
-            absent_tags_ids: {
-                let mut absent_tags_ids_and_objects_count: Vec<(Id, u32)> = Vec::new();
-                for tag in absent_tags {
-                    let tag_id = tag.get_id();
-                    if let Some(tag_objects_count) = self
-                        .database_write_transaction
-                        .get::<Id, u32>(TAG_TO_OBJECTS_COUNT, &tag_id)?
-                    {
-                        absent_tags_ids_and_objects_count.push((tag_id, tag_objects_count));
-                    }
-                }
-                absent_tags_ids_and_objects_count
-                    .sort_by_key(|(_, tag_objects_count)| *tag_objects_count);
-                absent_tags_ids_and_objects_count.reverse();
-                absent_tags_ids_and_objects_count
-                    .into_iter()
-                    .map(|(tag, _)| tag)
-                    .collect()
-            },
-            present_tags_ids: {
-                let mut present_tags_ids_and_objects_count: Vec<(Id, u32)> = Vec::new();
-                for tag in present_tags {
-                    let tag_id = tag.get_id();
-                    present_tags_ids_and_objects_count.push((
-                        tag_id.clone(),
-                        self.database_write_transaction
-                            .get::<Id, u32>(TAG_TO_OBJECTS_COUNT, &tag_id)?
-                            .unwrap_or(0 as u32),
-                    ));
-                }
-                present_tags_ids_and_objects_count
-                    .sort_by_key(|(_, tag_objects_count)| *tag_objects_count);
-                present_tags_ids_and_objects_count
-                    .into_iter()
-                    .map(|(tag, _)| tag)
-                    .collect()
-            },
-            start_after_object,
-            cursors: Vec::new(),
-            index_1: 0 as usize,
-            index_2: 1 as usize,
-            end: false,
-        }))
-    }
 }
 
 struct Cursor<'a> {
@@ -308,7 +255,7 @@ impl<'a> Cursor<'a> {
 }
 
 pub struct SearchIterator<'a> {
-    database_write_transaction: &'a lawn::database::WriteTransaction<'a>,
+    database_transaction: &'a lawn::database::ReadTransaction<'a>,
     present_tags_ids: Vec<Id>,
     absent_tags_ids: Vec<Id>,
     start_after_object: Option<Id>,
@@ -342,7 +289,7 @@ impl<'a> FallibleIterator for SearchIterator<'a> {
                     )
                     .all(|tag_id| {
                         Ok(self
-                            .database_write_transaction
+                            .database_transaction
                             .get::<(Id, Id), [u8; 0]>(
                                 TAG_AND_OBJECT,
                                 &(tag_id.clone(), first_cursor_object.clone()),
@@ -373,7 +320,7 @@ impl<'a> FallibleIterator for SearchIterator<'a> {
                 && self.cursors.len() <= self.index_1
             {
                 let mut cursor =
-                    Cursor::new(self.database_write_transaction.iter::<(Id, Id), [u8; 0]>(
+                    Cursor::new(self.database_transaction.iter::<(Id, Id), [u8; 0]>(
                         TAG_AND_OBJECT,
                         Some(&(
                             self.present_tags_ids[self.index_1].clone(),
@@ -410,7 +357,7 @@ impl<'a> FallibleIterator for SearchIterator<'a> {
                 && self.cursors.len() <= self.index_2
             {
                 let cursor = Cursor::new(
-                    self.database_write_transaction.iter::<(Id, Id), [u8; 0]>(
+                    self.database_transaction.iter::<(Id, Id), [u8; 0]>(
                         TAG_AND_OBJECT,
                         Some(&(
                             self.present_tags_ids[self.index_2].clone(),
@@ -436,7 +383,100 @@ impl<'a> FallibleIterator for SearchIterator<'a> {
                 }
                 self.cursors.push(cursor);
             }
+
+            while self.cursors[self.index_2].current_value.as_ref().unwrap().1
+                < self.cursors[self.index_1].current_value.as_ref().unwrap().1
+            {
+                self.cursors[self.index_2].next()?;
+                if !self.cursors[self.index_2]
+                    .current_value
+                    .as_ref()
+                    .is_some_and(|current_value| {
+                        current_value.0 == self.present_tags_ids[self.index_2]
+                    })
+                {
+                    self.end = true;
+                    return Ok(None);
+                }
+            }
+            if self.cursors[self.index_2].current_value.as_ref().unwrap().1
+                == self.cursors[self.index_1].current_value.as_ref().unwrap().1
+            {
+                self.index_1 = (self.index_1 + 1) % self.present_tags_ids.len();
+                self.index_2 = (self.index_2 + 1) % self.present_tags_ids.len();
+            } else {
+                while self.cursors[0].current_value.as_ref().unwrap().1
+                    < self.cursors[self.index_2].current_value.as_ref().unwrap().1
+                {
+                    self.cursors[0].next()?;
+                    if !self.cursors[0]
+                        .current_value
+                        .as_ref()
+                        .is_some_and(|current_value| current_value.0 == self.present_tags_ids[0])
+                    {
+                        self.end = true;
+                        return Ok(None);
+                    }
+                }
+                self.index_1 = 0;
+                self.index_2 = 0;
+            }
         }
+    }
+}
+
+impl<'a> ReadTransaction<'a> {
+    pub fn search(
+        &self,
+        present_tags: &Vec<Object>,
+        absent_tags: &Vec<Object>,
+        start_after_object: Option<Id>,
+    ) -> Result<Box<dyn FallibleIterator<Item = Id, Error = String> + '_>, String> {
+        Ok(Box::new(SearchIterator {
+            database_transaction: &self.database_read_transaction,
+            absent_tags_ids: {
+                let mut absent_tags_ids_and_objects_count: Vec<(Id, u32)> = Vec::new();
+                for tag in absent_tags {
+                    let tag_id = tag.get_id();
+                    if let Some(tag_objects_count) = self
+                        .database_read_transaction
+                        .get::<Id, u32>(TAG_TO_OBJECTS_COUNT, &tag_id)?
+                    {
+                        absent_tags_ids_and_objects_count.push((tag_id, tag_objects_count));
+                    }
+                }
+                absent_tags_ids_and_objects_count
+                    .sort_by_key(|(_, tag_objects_count)| *tag_objects_count);
+                absent_tags_ids_and_objects_count.reverse();
+                absent_tags_ids_and_objects_count
+                    .into_iter()
+                    .map(|(tag, _)| tag)
+                    .collect()
+            },
+            present_tags_ids: {
+                let mut present_tags_ids_and_objects_count: Vec<(Id, u32)> = Vec::new();
+                for tag in present_tags {
+                    let tag_id = tag.get_id();
+                    present_tags_ids_and_objects_count.push((
+                        tag_id.clone(),
+                        self.database_read_transaction
+                            .get::<Id, u32>(TAG_TO_OBJECTS_COUNT, &tag_id)?
+                            .unwrap_or(0 as u32),
+                    ));
+                }
+                present_tags_ids_and_objects_count
+                    .sort_by_key(|(_, tag_objects_count)| *tag_objects_count);
+                present_tags_ids_and_objects_count
+                    .into_iter()
+                    .map(|(tag, _)| tag)
+                    .collect()
+            },
+            start_after_object,
+            cursors: Vec::new(),
+            index_1: 0 as usize,
+            index_2: 1 as usize,
+            end: false,
+        }))
     }
 }
 
