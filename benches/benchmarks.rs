1f0f63edbdba5b7b5389fad60d710bccfe76ffa7
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-12-14 20:10:22 +0300

    move all benchmark configuration to YAML

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -3,24 +3,33 @@ use fallible_iterator::FallibleIterator;
 use nanorand::{Rng, WyRand};
 use std::{fs, io::BufReader};
 
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
 extern crate dream;
 use dream::*;
 
-fn criterion_benchmark(c: &mut Criterion) {
-    const TOTAL_TAGS_COUNT: usize = 100;
-    const OBJECT_TAGS_COUNT: usize = 10;
-    const OBJECTS_COUNT: usize = 100000;
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+struct Config {
+    index: IndexConfig,
+    rng_seed: u64,
+    total_tags_count: usize,
+    object_tags_count: usize,
+    objects_count: usize,
+    benchmark_in_memory: bool,
+    benchmark_on_disk: bool,
+}
 
-    let mut rng = WyRand::new_seed(0);
-    let mut index = Index::new(
-        serde_saphyr::from_reader(BufReader::new(
-            fs::File::open("benches/index_config.yml").unwrap(),
-        ))
-        .unwrap(),
-    )
+#[cfg(feature = "serde")]
+fn criterion_benchmark(bencher_context: &mut Criterion) {
+    let config: Config = serde_saphyr::from_reader(BufReader::new(
+        fs::File::open("benches/config.yml").unwrap(),
+    ))
     .unwrap();
 
-    let mut tags = (0..TOTAL_TAGS_COUNT)
+    let mut index = Index::new(config.index).unwrap();
+    let mut rng = WyRand::new_seed(config.rng_seed);
+    let mut tags = (0..config.total_tags_count)
         .map(|_| {
             let mut tag = vec![0u8; 16];
             rng.fill(&mut tag);
@@ -29,10 +38,10 @@ fn criterion_benchmark(c: &mut Criterion) {
         .collect::<Vec<_>>();
     index
         .lock_all_and_write(|transaction| {
-            for _ in 0..OBJECTS_COUNT {
+            for _ in 0..config.objects_count {
                 let mut object_value = vec![0u8; 16];
                 rng.fill(&mut object_value);
-                let mut tags = (0..OBJECT_TAGS_COUNT)
+                let mut tags = (0..config.object_tags_count)
                     .map(|_| tags[rng.generate_range(0..tags.len())].clone())
                     .collect::<Vec<_>>();
                 tags.sort();
@@ -41,35 +50,62 @@ fn criterion_benchmark(c: &mut Criterion) {
             }
             Ok(())
         })
-        .unwrap()
-        .database
-        .lock_all_and_checkpoint()
         .unwrap();
 
-    for search_tags_count in 1..=4 {
-        c.bench_function(
-            format!("search by {search_tags_count} tags").as_str(),
-            |b| {
-                b.iter_batched(
-                    || {
-                        rng.shuffle(&mut tags);
-                        tags.iter()
-                            .take(search_tags_count)
-                            .cloned()
-                            .collect::<Vec<_>>()
-                    },
-                    |present_tags| {
-                        index.lock_all_writes_and_read(|transaction| {
-                            transaction
-                                .search(&present_tags, &vec![], None)?
-                                .collect::<Vec<_>>()?;
-                            Ok(())
-                        })
-                    },
-                    BatchSize::SmallInput,
-                );
-            },
-        );
+    if config.benchmark_in_memory {
+        for search_tags_count in 1..=4 {
+            bencher_context.bench_function(
+                format!("in-memory: searching all objects by {search_tags_count} tags").as_str(),
+                |bencher| {
+                    bencher.iter_batched(
+                        || {
+                            rng.shuffle(&mut tags);
+                            tags.iter()
+                                .take(search_tags_count)
+                                .cloned()
+                                .collect::<Vec<_>>()
+                        },
+                        |present_tags| {
+                            index.lock_all_writes_and_read(|transaction| {
+                                transaction
+                                    .search(&present_tags, &vec![], None)?
+                                    .collect::<Vec<_>>()?;
+                                Ok(())
+                            })
+                        },
+                        BatchSize::SmallInput,
+                    );
+                },
+            );
+        }
+    }
+    if config.benchmark_on_disk {
+        index.database.lock_all_and_checkpoint().unwrap();
+        for search_tags_count in 1..=4 {
+            bencher_context.bench_function(
+                format!("on-disk: searching all objects by {search_tags_count} tags").as_str(),
+                |bencher| {
+                    bencher.iter_batched(
+                        || {
+                            rng.shuffle(&mut tags);
+                            tags.iter()
+                                .take(search_tags_count)
+                                .cloned()
+                                .collect::<Vec<_>>()
+                        },
+                        |present_tags| {
+                            index.lock_all_writes_and_read(|transaction| {
+                                transaction
+                                    .search(&present_tags, &vec![], None)?
+                                    .collect::<Vec<_>>()?;
+                                Ok(())
+                            })
+                        },
+                        BatchSize::SmallInput,
+                    );
+                },
+            );
+        }
     }
 }
 
